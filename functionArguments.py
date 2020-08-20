364d2151849a393eeb73d1187c361e0f884d7379
Mohaimen Hassan <mohaimenhassan08@gmail.com>
2020-08-20 18:24:03 -0400

    Functions and arguments edited

--- functionArguments.py
+++ functionArguments.py
@@ -4,3 +4,11 @@ def arguments(first, second=2, third=3):
     print(third)
 
 arguments(10,third=30, second=20)
+
+def argumentsStrict(first, /, second=2, *, third=3): # Before /, positional only, after *, keyword only. In between is both.
+    print(first)
+    print(second)
+    print(third)
+
+argumentsStrict(10, third=30, second=20)
+
